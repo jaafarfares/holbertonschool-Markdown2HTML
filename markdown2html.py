6e24f15384b6d2153e29e2ab38b0c0f0ee70edb7
jaafar <jaafarferes@gmail.com>
2022-12-24 13:14:14 +0100

    Write a script markdown2html.py that takes an argument 2 strings:

--- markdown2html.py
+++ markdown2html.py
@@ -13,17 +13,11 @@ if __name__ == "__main__":
     n = len(sys.argv)
 
     if (n < 2):
-        """
-        my first condition
-        """
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
     if (not path.exists("README.md")):
-        """
-        my seconde condition
-        """
-        sys.stderr.write("Missing "+str(sys.argv[1]))
+        sys.stderr.write("Missing "+str(sys.argv[1]) + "\n")
         exit(1)
 
     else:
